07494f7bdc40dbce328a70b62f1b73efb998ed29
Ramo <60301473+ramy-zemo@users.noreply.github.com>
2021-03-15 01:07:54 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+DISCORD_TOKEN = ""
+ADMIN_IDS = []
+ICON_URL = "https://www.zemodesign.at/wp-content/uploads/2020/05/Favicon-BL-BG.png"
+
+DB_IP = ""
+DB_USER = ""
+DB_PASSWORD = ""
+DB_DATABASE = ""
+
+TWITCH_CLIENT_ID = ""
+TWITCH_CLIENT_SECRET = ""
+
+BITLY_API = ""
